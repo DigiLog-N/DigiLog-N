a80e7617c860097baf328d8b2fb00fb0316f54e7
Charles Cowart <charlie@digilog-n.canvasslabs.com>
2020-10-18 21:44:54 -0700

    cleanup

--- test_email_notification.py
+++ test_email_notification.py
@@ -1,67 +1,10 @@
 import pyarrow as pa
-import pandas as pd
-import pyspark
-from pyspark.sql.functions import col, pandas_udf
-from pyspark.sql.types import LongType
-from pyspark.sql import SparkSession
-from PlasmaReader import PlasmaReader
-from DataSource import DataSource
-from DataSourceRegistry import DataSourceRegistry
 from PlasmaReader import PlasmaReader
 from GMailUser import GMailUser
-import pyarrow as pa
 import pyarrow.plasma as plasma
 import numpy as np
-import random
 from random import choice
 from string import digits
-
-
-def read_test(plasma_file_path, key_prefix):
-    # Use our PlasmaReader object to read all the data currently in Plasma and
-    # convert it into a Pandas DataFrame.
-    pdf_list = []
-
-    pr = PlasmaReader(plasma_file_path, key_prefix)
-    pdf = pr.to_pandas()
-    print(pdf.info())
-    print(pdf.shape)
-    pdf.to_csv('./test.csv')
-    pdf_list.append(pdf)
-
-    # Use it again to see if there's any new data. Since this is loaded from
-    # a fixed file, there won't be.
-    pdf = pr.to_pandas()
-    if pdf:
-        print(pdf.info())
-        print(pdf.shape)
-        pdf.to_csv('./test2.csv')
-        pdf_list.append(pdf)
-    else:
-        print("no new data!")
-
-    # return the list of Pandas DataFrames to be used in a possible write-test.
-    return pdf_list
-
-
-def write_test(pandas_data_frame):
-    spark = SparkSession.builder.getOrCreate()
-
-    # tests show this option really does improve times:
-    # from nearly 5 minutes down to 20 seconds for 10,000,000 rows.
-    spark.conf.set("spark.sql.execution.arrow.pyspark.enabled", "true")
-    # for now, throw an error, rather than fallback to the non-arrow option.
-    spark.conf.set("spark.sql.execution.arrow.pyspark.fallback.enabled", "false")
-    # these I don't know much about, but it was recommended.
-    spark.conf.set("spark.sql.parquet.mergeSchema", "false")
-    spark.conf.set("spark.hadoop.parquet.enable.summary-metadata", "false")
-
-    def add_func(a, b):
-        return a + b
-
-    add_me = pandas_udf(add_func, returnType=LongType())
-    sdf = spark.createDataFrame(pandas_data_frame)
-    sdf.select(add_me(col("op1"), col("op2"))).show()
 
 
 def email_main(user, password, subject, message, addressed_to):
@@ -91,7 +34,6 @@ def email_main(user, password, subject, message, addressed_to):
         #  to prevent downstream code from having to know/handle all of these very specific errors.
         #  Yet, we want to be fairly robust; we need to know if a user did not receive an email.
         raise ValueError("Server replied w/unexpected error code: %s" % str(e))
-
 
 
 class WriteToPlasma:
